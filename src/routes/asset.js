02583ddca4dd72f6415f7fcac30f8e14e12f1a82
Cela Pablo <pablooandress9@gmail.com>
2024-01-11 12:52:43 -0300

    updates responses for /assets

--- src/routes/asset.js
+++ src/routes/asset.js
@@ -32,20 +32,14 @@ router.get('/assets/:network', async (req, res) => {
     try {
         const { network } = req.params;
         const { minters } = await sweep.getMinters(network);
+        const promises = [];
 
-        const allDataResults = await Promise.all(
-            minters.map(async (minter) => ({
-                [minter]: await asset.fetchData(network, minter)
-            }))
-        );
+        minters.forEach((minter) => {
+            promises.push(fetchDataForMinter(network, minter));
+        });
 
-        const response = allDataResults.reduce((acc, result) => {
-            const minter = Object.keys(result)[0];
-            acc[minter] = { ...result[minter] };
-            return acc;
-        }, {});
-
-        res.json({ [network]: response });
+        const data = await Promise.all(promises);
+        res.json({ response: data });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -58,36 +52,26 @@ router.get('/assets', async (req, res) => {
         }));
 
         const allMinters = await Promise.all(allMintersPromises);
-        const allDataPromises = allMinters.map(async (item) => {
+
+        const promises = [];
+        allMinters.forEach((item) => {
             const [network, { minters }] = Object.entries(item)[0];
-            return processMinters(network, minters);
+            minters.forEach((minter) => {
+                promises.push(fetchDataForMinter(network, minter));
+            });
         });
 
-        const response = await Promise.all(allDataPromises);
-        const responseParsed = processResponse(response);
-        res.json({ response: responseParsed });
+        const data = await Promise.all(promises);
+        res.json({ response: data });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
-async function processMinters(network, minters) {
-    const promises = minters.map(async(minter) => await asset.fetchData(network, minter));
-    return { ...await Promise.all(promises), network };
-}
-
-function processResponse(resp) {
-    var data = []
-    resp.forEach(network => {
-        Object.keys(network).forEach(key => {
-            if(key !== "network") {
-                const newElement = {...network[key], network: network["network"]};
-                data.push(newElement)
-            }
-        })
-    });
-
-    return data;
+// ------------------------------------------------
+async function fetchDataForMinter(network, minter) {
+    const assetData = await asset.fetchData(network, minter);
+    return { ...assetData, network, asset: minter };
 }
 
 module.exports = router;
